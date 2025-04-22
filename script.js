9dc43ab78e63b19c1c89f9461d2653b4bfd90f8d
M-coder-Q <masoomaqasim5@icloud.com>
2025-04-22 20:00:09 +0500

    Update script.js

--- script.js
+++ script.js
@@ -3,18 +3,18 @@
 const display = document.getElementById("display");
 
 function appendToDisplay(input){
-  display.value += input;
+    display.value += input;
 }
 
 function clearDisplay(){
-  display.value = "";
+    display.value = "";
 }
 
 function calculate(){
-  try{
-      diplay.value = eval(display.valve);
-  }
-  catch(error){
-    diplay.value = "Error";
-  }
+    try{
+        display.value = eval(display.value);
+    }
+    catch(error){
+        display.value = "Error";
+    }
 }
